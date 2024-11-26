65e73c8931f634f804153ccebcfade5754ed9206
Vjaraes <141291433+Vjaraes@users.noreply.github.com>
2024-11-26 12:05:41 -0300

    continuacion
    continuacion

---
+++ create-account.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">  
+    <link rel="stylesheet" href="css/main.css">  
+    <link rel="stylesheet" href="css/signup.css">
+        
+    <title>Crear una Cuenta</title>
+    <style>
+        .container{
+            animation: transitionIn-X 0.5s;
+        }
+    </style>
+</head>
+<body>
+<?php
+ 
+
+session_start();
+
+$_SESSION["user"]="";
+$_SESSION["usertype"]="";
+
+//Zona Horaria
+date_default_timezone_set('Santiago/America');
+$date = date('d-m-Y');
+
+$_SESSION["date"]=$date;
+
+
+
+include("connection.php");
+
+
+
+
+
+if($_POST){
+
+    $result= $database->query("select * from webuser");
+
+    $fname=$_SESSION['personal']['fname'];
+    $lname=$_SESSION['personal']['lname'];
+    $name=$fname." ".$lname;
+    $address=$_SESSION['personal']['address'];
+    $rut=$_SESSION['personal']['rut'];
+    $dob=$_SESSION['personal']['dob'];
+    $email=$_POST['newemail'];
+    $tele=$_POST['tele'];
+    $newpassword=$_POST['newpassword'];
+    $cpassword=$_POST['cpassword'];
+    
+    if ($newpassword==$cpassword){
+        $sqlmain= "select * from webuser where email=?;";
+        $stmt = $database->prepare($sqlmain);
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows==1){
+            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Ya existe una cuenta asociada a este correo.</label>';
+        }else{
+            
+            $database->query("insert into patient(pemail,pname,ppassword, paddress, pnic,pdob,ptel) values('$email','$name','$newpassword','$address','$rut','$dob','$tele');");
+            $database->query("insert into webuser values('$email','p')");
+
+            //print_r("insert into patient values($pid,'$email','$fname','$lname','$newpassword','$address','$rut','$dob','$tele');");
+            $_SESSION["user"]=$email;
+            $_SESSION["usertype"]="p";
+            $_SESSION["username"]=$fname;
+
+            header('Location: patient/index.php');
+            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;"></label>';
+        }
+        
+    }else{
+        $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Clave invalida: Por favor intente nuevamente.</label>';
+    }
+
+
+
+    
+}else{
+    //header('location: signup.php');
+    $error='<label for="promter" class="form-label"></label>';
+}
+
+?>
+
+
+    <center>
+    <div class="container">
+        <table border="0" style="width: 69%;">
+            <tr>
+                <td colspan="2">
+                    <p class="header-text">Comencemos</p>
+                    <p class="sub-text">Crea tu cuenta de usuario.</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td" colspan="2">
+                    <label for="newemail" class="form-label">Email: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="email" name="newemail" class="input-text" placeholder="Direccion de correo" required>
+                </td>
+                
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="tele" class="form-label">Celular: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="tel" name="tele" class="input-text"  placeholder="ex: 988888888" pattern="[9]{1}[0-9]{8}" >
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="newpassword" class="form-label">Crea un nuevo Password: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="password" name="newpassword" class="input-text" placeholder="Password nuevo" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="cpassword" class="form-label">Confirme su Password: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="password" name="cpassword" class="input-text" placeholder="Confirme su Password" required>
+                </td>
+            </tr>
+     
+            <tr>
+                
+                <td colspan="2">
+                    <?php echo $error ?>
+
+                </td>
+            </tr>
+            
+            <tr>
+                <td>
+                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
+                </td>
+                <td>
+                    <input type="submit" value="Sign Up" class="login-btn btn-primary btn">
+                </td>
+
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Cuenta ya existente.&#63; </label>
+                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                    <br><br><br>
+                </td>
+            </tr>
+
+                    </form>
+            </tr>
+        </table>
+
+    </div>
+</center>
+</body>
+</html>
